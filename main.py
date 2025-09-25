d14813f6e960aff020bffa0b01de7a0d31881aba
irugawolf <sergiorebellatof@gmail.com>
2025-09-25 13:44:09 -0300

    Fully fleshed out Bookbot!

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from stats import words, characters
+from stats import words, characters, build_sorted_char_list
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    text = get_book_text(book_path)
     count = words(text)
-    print(f"Found {count} total words")
     letters = characters(text)
-    print(letters)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    sorted_list = build_sorted_char_list(letters)
+    for item in sorted_list:
+        ch = item["char"]
+        if ch.isalpha():
+            print(f"{ch}: {item['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,13 @@ def characters(text):
         else:
             letters[ch] = 1
     return letters
+
+def sort_on(item):
+    return item["num"]
+
+def build_sorted_char_list(dictionary):
+    result = []
+    for ch, num in dictionary.items():
+        result.append({"char": ch, "num": num})
+    result.sort(key=sort_on, reverse = True)
+    return result
